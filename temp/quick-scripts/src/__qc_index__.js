a4532f5f3561b05b617e26ed4a236cf267abd558
董涛 <7737072+dongtaoZZ@user.noreply.gitee.com>
2021-02-02 21:54:43 +0800

    删除文件 temp

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,8 +0,0 @@
-
-require('./assets/Script/AudioManager');
-require('./assets/Script/AutoRotationSc');
-require('./assets/Script/BallSc');
-require('./assets/Script/Component/BannerShow');
-require('./assets/Script/GameSc');
-require('./assets/Script/Modules/GSdk');
-require('./assets/Script/WaterEffectSc');
